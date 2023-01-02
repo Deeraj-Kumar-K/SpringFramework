a51e2493adfdddfbd78c4b773e5373157bf11a0a
Deeraj Kumar <deerajkmr98@gmail.com>
2023-01-02 22:45:19 +0530

    initial commit

---
+++ peopledb-web/src/test/java/kumar/deeraj/peopledbweb/PeopledbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kumar.deeraj.peopledbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopledbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
